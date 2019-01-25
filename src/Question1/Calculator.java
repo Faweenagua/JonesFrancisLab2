383704b479baa60ff6d5aedea99fcda36e52c3cd
Faweenagua <46810972+Faweenagua@users.noreply.github.com>
2019-01-25 10:15:53 +0000

    k

---
+++ src/Question1/Calculator.java
@@ -0,0 +1,26 @@
+package Question1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    double firstNum;
+    double secondNum;
+    double ans;
+    String name;
+
+    public Calculator(){
+
+    }
+
+    public Calculator(String calName){
+        this.name = calName;
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
